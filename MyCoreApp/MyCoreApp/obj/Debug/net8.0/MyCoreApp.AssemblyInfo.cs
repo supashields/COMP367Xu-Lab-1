031c06476d481b8913f1e451fb03bb7ac340135a
JohnX <johnxu27@gmail.com>
2024-02-04 01:45:07 -0500

    modified code and added gitignore

--- MyCoreApp/MyCoreApp/obj/Debug/net8.0/MyCoreApp.AssemblyInfo.cs
+++ MyCoreApp/MyCoreApp/obj/Debug/net8.0/MyCoreApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe1b910482da44a7d7ee9f645814e99b504804e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
