fe1b910482da44a7d7ee9f645814e99b504804e5
jiuqiangxu <johnxu27@gmail.com>
2024-02-03 23:59:34 -0500

    first commit

---
+++ MyCoreApp/MyCoreApp/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
+
+namespace MyCoreApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            string dateTime = DateTime.Now.ToString("d", new CultureInfo("en-US"));
+            ViewData["TimeStamp"] = dateTime;
+        }
+    }
+}
